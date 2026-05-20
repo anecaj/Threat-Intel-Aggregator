e24d3b33bbc7fd71fc157c7a31f7344185128af3
anecaj <an9905@g.rit.edu>
2026-05-20 19:31:05 -0400

    Fix base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
-  base: "/threat-intel-aggregator/",
+  base: "/Threat-Intel-Aggregator/",
   server: {
     proxy: {
       "/api": {
